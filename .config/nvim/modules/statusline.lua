efcc9dded1c3455be31250a8374ef98eb11b566c
bogus <nldev00@pm.me>
2024-10-12 06:09:20 -0500

    nvim: adjust status line

--- .config/nvim/modules/statusline.lua
+++ .config/nvim/modules/statusline.lua
@@ -25,11 +25,11 @@ local module = {
     -- buffer number
     vim.opt.statusline:append('B:%n ')
 
-    -- current line / total lines
-    vim.opt.statusline:append('L:%l/%L ')
-
     -- column number
     vim.opt.statusline:append('C:%v ')
+
+    -- current line / total lines
+    vim.opt.statusline:append('L:%l/%L ')
 
     -- percentage through the file
     vim.opt.statusline:append('[%p%%]')
